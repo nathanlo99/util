5134a245ac0782c32a9ce401921572a1eff3152f
Nathan Lo <nathanlo99@hotmail.ca>
2017-12-06 03:38:35 -0500

    Formatted string.h

--- string.h
+++ string.h
@@ -1,4 +1,3 @@
-
 #ifndef UTIL_STRING
 #define UTIL_STRING
 
@@ -7,97 +6,100 @@
 
 // Defines a heap-allocated c-string
 namespace util {
-    class string {
-    private:
-        char* s = NULL;
-        size_t size = 0;
-    public:
-        inline string(): s(NULL), size(0) {}
+class string {
+private:
+  char *s = NULL;
+  size_t size = 0;
 
-        inline string(const char* str) {
-            if (str == NULL) {
-                s = NULL;
-                size = 0;
-            } else {
-                size = strlen(str);
-                s = (char*)malloc(size * sizeof(char));
-                strcpy(s, str);
-            }
-        }
+public:
+  inline string() : s(NULL), size(0) {}
 
-        inline string(const string& str) {
-            string(str.c_str());
-        }
+  inline string(const char *str) {
+    if (str == NULL) {
+      s = NULL;
+      size = 0;
+    } else {
+      size = strlen(str);
+      s = (char *)malloc(size * sizeof(char));
+      strcpy(s, str);
+    }
+  }
 
-        inline string(const char c) {
-            size = 1;
-            s = (char*)malloc(size * sizeof(char));
-            *s = c;
-        }
+  inline string(const string &str) { string(str.c_str()); }
 
-        inline ~string(){
-            if (s != NULL) free(s);
-        }
+  inline string(const char c) {
+    size = 1;
+    s = (char *)malloc(size * sizeof(char));
+    *s = c;
+  }
 
-        inline string& operator= (const char* str) {
-            if (str == NULL) {
-                if (s == NULL) free(s);
-                size = 0;
-            } else {
-                size = strlen(str);
-                s = (char*)realloc(s, size * sizeof(char));
-                strcpy(s, str);
-            }
-            return *this;
-        }
+  inline ~string() {
+    if (s != NULL)
+      free(s);
+  }
 
-        inline const string operator+(const char* str) const {
-            const int new_size = size + strlen(str);
-            char* ss = (char*)malloc(new_size * sizeof(char));
-            strcpy(ss, s);
-            strcat(ss, str);
-            const string res(ss);
-            return res;
-        }
+  inline string &operator=(const char *str) {
+    if (str == NULL) {
+      if (s == NULL)
+        free(s);
+      size = 0;
+    } else {
+      size = strlen(str);
+      s = (char *)realloc(s, size * sizeof(char));
+      strcpy(s, str);
+    }
+    return *this;
+  }
 
-        inline string& operator= (const string& rhs) {
-            if (this != &rhs)
-                *this = rhs.c_str();
-            return *this;
-        }
+  inline const string operator+(const char *str) const {
+    const int new_size = size + strlen(str);
+    char *ss = (char *)malloc(new_size * sizeof(char));
+    strcpy(ss, s);
+    strcat(ss, str);
+    const string res(ss);
+    return res;
+  }
 
-        inline string& operator+= (const string& rhs) {
-            *this = *this + rhs;
-            return *this;
-        }
+  inline string &operator=(const string &rhs) {
+    if (this != &rhs)
+      *this = rhs.c_str();
+    return *this;
+  }
 
-        inline string& operator+= (const char* str) {
-            *this = *this + str;
-            return *this;
-        }
+  inline string &operator+=(const string &rhs) {
+    *this = *this + rhs;
+    return *this;
+  }
 
-        inline const string operator+(const string& other) const {
-            return *this + other.c_str();
-        }
+  inline string &operator+=(const char *str) {
+    *this = *this + str;
+    return *this;
+  }
 
-        inline bool operator== (const string& other) const {
-            if (s == NULL || other.c_str() == NULL) return 0;
-            if (size != other.length()) return 0;
-            return strcmp(s, other.c_str()) == 0;
-        }
+  inline const string operator+(const string &other) const {
+    return *this + other.c_str();
+  }
 
-        inline bool operator!= (const string& other) const {
-            return !(*this == other);
-        }
+  inline bool operator==(const string &other) const {
+    if (s == NULL || other.c_str() == NULL)
+      return 0;
+    if (size != other.length())
+      return 0;
+    return strcmp(s, other.c_str()) == 0;
+  }
 
-        inline int length() const { return size; }
-        inline char* c_str() const { return s; }
+  inline bool operator!=(const string &other) const {
+    return !(*this == other);
+  }
 
-        friend std::ostream& operator<< (std::ostream& os, const string& str) {
-            return os << str.s;
-        }
+  inline int length() const { return size; }
+  inline char *c_str() const { return s; }
 
-    }; // class string
+  friend std::ostream &operator<<(std::ostream &os, const string &str) {
+    return os << str.s;
+  }
+
+}; // class string
 
 } // namespace util
 
