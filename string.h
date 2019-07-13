c381cb758e38fd4d4b073d2b6a8f563da2cefa8f
Nathan Lo <nathanlo99@hotmail.ca>
2019-07-12 21:18:56 -0400

    Trying to make string (basic_string) C++14 compliant

--- string.h
+++ string.h
@@ -21,184 +21,78 @@ static constexpr unsigned int roundup(unsigned int v) {
 
 // Defines a heap-allocated c-string
 namespace util {
-class string {
-  using value_type = char;
-  using traits_type = std::char_traits<char>;
-  using allocator_type = std::allocator<char>;
-  using reference = char &;
-  using const_reference = const char &;
-  using pointer = char *;
-  using const_pointer = const char *;
-  using iterator = char *;
-  using const_iterator = char *;
-  using reverse_iterator = std::reverse_iterator<char *>;
-  using const_reverse_iterator = std::reverse_iterator<char *>;
-  using difference_type = ptrdiff_t;
-  using size_type = size_t;
-  static constexpr size_type npos = std::numeric_limits<size_type>::max();
+template <class charT, class traits = char_traits<charT>,
+          class Alloc = allocator<charT>>
+class basic_string {
+  using value_type = charT;
+  using traits_type = std::char_traits<value_type>;
+  using allocator_type = std::allocator<value_type>;
+  using reference = value_type &;
+  using const_reference = const value_type &;
+  using pointer = allocator_traits<allocator_type>::pointer;
+  using const_pointer = allocator_traits<allocator_type>::const_pointer;
+  using iterator = value_type *;
+  using const_iterator = const value_type *;
+  using reverse_iterator = std::reverse_iterator<iterator>;
+  using const_reverse_iterator = std::reverse_iterator<iterator>;
+  using difference_type = allocator_traits<allocator_type>::difference_type;
+  using size_type = allocator_traits<allocator_type>::size_type;
+  static constexpr size_type char_width = sizeof(charT);
+  static constexpr size_type npos = -1;
 
-  size_t m_size, m_cap;
-  char *m_data;
-
-  string(size_t size, size_t cap, char *data)
-      : m_size{size}, m_cap{cap}, m_data{data} {}
+  size_type m_size, m_cap;
+  allocator_type m_alloc;
+  pointer m_data;
 
 public:
   // Default constructor
-  string() {
-    m_size = 0;
-    m_cap = roundup(m_size + 1);
-    m_data = (char *)malloc(m_cap);
-    if (!m_data)
-      throw std::bad_alloc{};
-  }
+  basic_string() : m_size{0}, m_cap{2}, m_alloc{}, m_data{m_alloc(m_cap)} {}
+  explicit basic_string(const allocator_type &alloc)
+      : m_size{0}, m_cap{2}, m_alloc{alloc}, m_data{m_alloc(m_cap)} {}
 
   // Copy constructor
-  string(const string &other) {
-    m_size = other.m_size;
-    m_cap = other.m_cap;
-    m_data = (char *)malloc(m_cap);
-    if (!m_data)
-      throw std::bad_alloc{};
-    for (size_t i = 0; i < m_size; ++i)
-      m_data[i] = other.m_data[i];
-  }
+  basic_string(const basic_string &str);
+  basic_string(const basic_string &str, const allocator_type &alloc);
 
   // Substring constructor
-  string(const string &str, size_t pos, size_t len = npos) {
-    if (pos >= str.size())
-      throw std::out_of_range{"pos out of range"};
-    if (len == npos || pos + len >= str.m_size)
-      len = str.m_size - pos;
-    m_size = len;
-    m_cap = roundup(m_size + 1);
-    m_data = (char *)malloc(m_cap);
-    if (!m_data)
-      throw std::bad_alloc{};
-    for (size_t i = 0; i < m_size; ++i)
-      m_data[i] = str.m_data[i + pos];
-  }
+  string(const string &str, size_t pos, size_t len = npos);
 
   // From C-string constructor
-  string(const char *str) {
-    m_size = strlen(str);
-    m_cap = roundup(m_size + 1);
-    m_data = (char *)malloc(m_cap);
-    if (!m_data)
-      throw std::bad_alloc{};
-    for (size_t i = 0; i < m_size; ++i)
-      m_data[i] = str[i];
-  }
+  string(const char *str);
 
   // Buffer constructor
-  string(const char *s, size_t n) {
-    m_size = n;
-    m_cap = roundup(m_size + 1);
-    m_data = (char *)malloc(m_cap);
-    if (!m_data)
-      throw std::bad_alloc{};
-    for (size_t i = 0; i < m_size; ++i)
-      m_data[i] = s[i];
-  }
+  string(const char *s, size_t n);
 
   // Fill constructor
-  string(size_t n, char c) {
-    m_size = n;
-    m_cap = roundup(m_size + 1);
-    m_data = (char *)malloc(m_cap);
-    if (!m_data)
-      throw std::bad_alloc{};
-    for (size_t i = 0; i < m_size; ++i)
-      m_data[i] = c;
-  }
+  string(size_t n, char c);
 
   // Range constructor
   template <class InputIterator>
-  string(InputIterator first, InputIterator last) {
-    m_size = std::distance(first, last);
-    m_cap = roundup(m_size + 1);
-    m_data = (char *)malloc(m_cap);
-    if (!m_data)
-      throw std::bad_alloc{};
-    char *data_it = m_data;
-    for (InputIterator it = first; it != last; ++it)
-      *data_it = *it;
-  }
+  string(InputIterator first, InputIterator last);
 
   // Initializer list constructor
-  string(std::initializer_list<char> il) {
-    m_size = il.size();
-    m_cap = roundup(m_size + 1);
-    m_data = (char *)malloc(m_cap);
-    if (!m_data)
-      throw std::bad_alloc{};
-    char *data_it = m_data;
-    for (auto c : il)
-      *data_it = c;
-  }
+  string(std::initializer_list<char> il);
 
   // Move constructor
-  string(string &&other) noexcept {
-    m_size = other.m_size;
-    m_cap = other.m_cap;
-    m_data = other.m_data;
-    other.m_data =
-        nullptr; // Under any other circumstances, m_data is not nullptr.
-                 // This is to make sure this->m_data is not freed.
-  }
+  string(string &&other) noexcept;
 
   // Destructor
   ~string() { free(m_data); }
 
   // string assign
-  string &operator=(const string &rhs) {
-    if (this != &rhs)
-      *this = rhs.c_str();
-    return *this;
-  }
+  string &operator=(const string &rhs);
 
   // c-string assign
-  string &operator=(const char *str) {
-    assert(str && "Tried to assign nullptr!");
-    m_size = strlen(str);
-    m_cap = roundup(m_size + 1);
-    m_data = (char *)realloc(m_data, m_cap);
-    for (size_t i = 0; i < m_size; ++i)
-      m_data[i] = str[i];
-    return *this;
-  }
+  string &operator=(const char *str);
 
   // character assign
-  string &operator=(char c) {
-    if (m_cap < 4) {
-      m_cap = 4;
-      m_data = (char *)realloc(m_data, m_cap);
-    }
-    m_size = 1;
-    m_data[0] = c;
-    return *this;
-  }
+  string &operator=(char c);
 
   // initializer_list assign
-  string &operator=(std::initializer_list<char> il) {
-    m_size = il.size();
-    m_cap = roundup(m_size + 1);
-    m_data = (char *)realloc(m_data, m_cap);
-    char *data_it = m_data;
-    for (auto c : il)
-      *data_it = c;
-    return *this;
-  }
+  string &operator=(std::initializer_list<char> il);
 
   // move assign
-  string &operator=(string &&str) noexcept {
-    free(m_data);
-    m_size = str.m_size;
-    m_cap = str.m_cap;
-    m_data = str.m_data;
-    str.m_data = nullptr; // See move constructor
-    return *this;
-  }
+  string &operator=(string &&str) noexcept;
 
   iterator begin() noexcept { return m_data; }
   const_iterator begin() const noexcept { return m_data; }
@@ -228,18 +122,7 @@ public:
   size_t length() const noexcept { return m_size; }
   size_t max_size() const noexcept { return npos; }
 
-  void resize(size_t n, char c = '\0') {
-    const size_t old_size = m_size;
-    if (n < m_size)
-      m_size = n;
-    else {
-      m_size = n;
-      m_cap = roundup(m_size + 1);
-      const char *new_data = m_data = (char *)realloc(m_data, m_cap);
-      for (size_t i = old_size; i < n; ++i)
-        m_data[i] = c;
-    }
-  }
+  void resize(size_t n, char c = '\0');
   size_t capacity() const noexcept { return m_cap; }
   void reserve(size_t n = 0) {
     if (n > m_cap) {
@@ -253,19 +136,10 @@ public:
     m_data = (char *)realloc(m_data, m_cap);
   }
   bool empty() const noexcept { return !m_size; }
-  void shrink_to_fit() {
-    m_cap = roundup(m_size + 1);
-    m_data = (char *)realloc(m_data, m_cap);
-  }
+  void shrink_to_fit();
 
-  char &operator[](size_t pos) {
-    m_data[m_size] = 0;
-    return m_data[pos];
-  }
-  const char &operator[](size_t pos) const {
-    m_data[m_size] = 0;
-    return m_data[pos];
-  }
+  char &operator[](size_t pos) { return m_data[pos]; }
+  const char &operator[](size_t pos) const { return m_data[pos]; }
   char &at(size_t pos) {
     if (pos >= m_size)
       throw std::out_of_range{"pos out of range"};
@@ -337,57 +211,97 @@ public:
   void swap(string &str);
   void pop_back() { --m_size; }
 
-  const string operator+(const char *str) const {
-    const size_t new_size = m_size + strlen(str);
-    size_t new_cap = roundup(new_size + 1);
-    char *new_data = (char *)malloc(new_cap);
-    // NOTE: not realloc as we want a new version of the string
-    for (size_t i = 0; i < m_size; ++i)
-      new_data[i] = m_data[i];
-    for (size_t i = m_size; i < new_size; ++i)
-      new_data[i] = str[i - m_size];
-    return string{new_size, new_cap, new_data};
-  }
+  const char *c_str() const;
+  const char *data() const;
 
-  // TODO: Verify that no optimizations are possible
-  // string &operator+=(const string &rhs) { return *this = *this + rhs; }
-  // string &operator+=(const char *str) { return *this = *this + str; }
+  allocator_type get_allocator() const noexcept;
 
-  const string operator+(const string &other) const {
-    return *this + other.m_data;
-  }
+  size_t copy(char *s, size_t len, size_t pos = 0) const;
+  size_t find(const string &str, size_t pos = 0) const noexcept;
+  size_t find(const char *s, size_t pos = 0) const;
+  size_t find(const char *s, size_t pos, size_type n) const;
+  size_t find(char c, size_t pos = 0) const noexcept;
 
-  bool operator==(const char *other) const {
-    if (!other || !m_data)
-      return 0;
-    for (size_t i = 0; i < m_size; ++i)
-      if (m_data[i] != other[i])
-        return 0;
-    if (other[m_size] != 0)
-      return 0;
-    return 1;
-  }
+  size_t rfind(const string &str, size_t pos = npos) const noexcept;
+  size_t rfind(const char *s, size_t pos = npos) const;
+  size_t rfind(const char *s, size_t pos, size_t n) const;
+  size_t rfind(char c, size_t pos = npos) const noexcept;
 
-  bool operator==(const string &other) const {
-    if (m_size != other.m_size)
-      return 0;
-    for (size_t i = 0; i < m_size; ++i)
-      if (m_data[i] != other.m_data[i])
-        return 0;
-    return 1;
-  }
+  size_t find_first_of(const string &str, size_t pos = 0) const noexcept;
+  size_t find_first_of(const char *s, size_t pos = 0) const;
+  size_t find_first_of(const char *s, size_t pos, size_t n) const;
+  size_t find_first_of(char c, size_t pos = 0) const noexcept;
 
-  bool operator!=(const string &other) const { return !(*this == other); }
+  size_t find_last_of(const string &str, size_t pos = npos) const noexcept;
+  size_t find_last_of(const char *s, size_t pos = npos) const;
+  size_t find_last_of(const char *s, size_t pos, size_t n) const;
+  size_t find_last_of(char c, size_t pos = npos) const noexcept;
 
-  const char *c_str() const {
-    assert(m_size + 1 < m_cap);
-    m_data[m_size] = 0;
-    return m_data;
-  }
+  size_t find_first_not_of(const string &str, size_t pos = 0) const noexcept;
+  size_t find_first_not_of(const char *s, size_t pos = 0) const;
+  size_t find_first_not_of(const char *s, size_t pos, size_t n) const;
+  size_t find_first_not_of(char c, size_t pos = 0) const noexcept;
+
+  size_t find_last_not_of(const string &str, size_t pos = npos) const noexcept;
+  size_t find_last_not_of(const char *s, size_t pos = npos) const;
+  size_t find_last_not_of(const char *s, size_t pos, size_t n) const;
+  size_t find_last_not_of(char c, size_t pos = npos) const noexcept;
+
+  string substr(size_t pos = 0, size_t len = npos) const;
+
+  int compare(const string &str) const noexcept;
+  int compare(size_t pos, size_t len, const string &str) const;
+  int compare(size_t pos, size_t len, const string &str, size_t subpos,
+              size_t sublen = npos) const;
+  int compare(const char *s) const;
+  int compare(size_t pos, size_t len, const char *s) const;
+  int compare(size_t pos, size_t len, const char *s, size_t n) const;
 
   friend std::ostream &operator<<(std::ostream &os, const string &str) {
     return os << str.c_str();
   }
 }; // class string
+
+string operator+(const string &lhs, const string &rhs);
+string operator+(string &&lhs, string &&rhs);
+string operator+(string &&lhs, const string &rhs);
+string operator+(const string &lhs, string &&rhs);
+string operator+(const string &lhs, const char *rhs);
+string operator+(string &&lhs, const char *rhs);
+string operator+(const char *lhs, const string &rhs);
+string operator+(const char *lhs, string &&rhs);
+string operator+(const string &lhs, char rhs);
+string operator+(string &&lhs, char rhs);
+string operator+(char lhs, const string &rhs);
+string operator+(char lhs, string &&rhs);
+
+bool operator==(const string &lhs, const string &rhs) noexcept;
+bool operator==(const char *lhs, const string &rhs);
+bool operator==(const string &lhs, const char *rhs);
+bool operator!=(const string &lhs, const string &rhs) noexcept;
+bool operator!=(const char *lhs, const string &rhs);
+bool operator!=(const string &lhs, const char *rhs);
+bool operator<(const string &lhs, const string &rhs) noexcept;
+bool operator<(const char *lhs, const string &rhs);
+bool operator<(const string &lhs, const char *rhs);
+bool operator<=(const string &lhs, const string &rhs) noexcept;
+bool operator<=(const char *lhs, const string &rhs);
+bool operator<=(const string &lhs, const char *rhs);
+bool operator>(const string &lhs, const string &rhs) noexcept;
+bool operator>(const char *lhs, const string &rhs);
+bool operator>(const string &lhs, const char *rhs);
+bool operator>=(const string &lhs, const string &rhs) noexcept;
+bool operator>=(const char *lhs, const string &rhs);
+bool operator>=(const string &lhs, const char *rhs);
+
+void swap(string &x, string &y);
+istream &operator>>(istream &is, string &str);
+ostream &operator<<(ostream &os, const string &str);
+
+istream &getline(istream &is, string &str, char delim);
+istream &getline(istream &&is, string &str, char delim);
+istream &getline(istream &is, string &str);
+istream &getline(istream &&is, string &str);
+
 } // namespace util
 #endif // #ifndef UTIL_STRING
